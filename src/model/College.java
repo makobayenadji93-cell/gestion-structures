99631931b2663a4e4bc6c2e450a3fecd6e9d844c
Makashi Uchiwa <makobayenadji93@gmail.com>
2026-06-01 11:47:00 +0100

    ajout de college.java(Messikia)

---
+++ src/model/College.java
@@ -0,0 +1,26 @@
+package model;
+import ui.ConsoleStyle;
+
+public class College extends StructureFormation {
+
+    public College(int id, String nom, String adresse,
+                   String telephone, String email,
+                   int anneeCreation) {
+
+        super(id, nom, adresse, telephone, email, anneeCreation);
+    }
+
+    @Override
+    public void afficherInfos() {
+        ConsoleStyle.ligneDbl();
+        ConsoleStyle.info("TYPE     :", "COLLEGE");
+        ConsoleStyle.ligneSep();
+        ConsoleStyle.info("ID       :", String.valueOf(id));
+        ConsoleStyle.info("Nom      :", nom);
+        ConsoleStyle.info("Adresse  :", adresse);
+        ConsoleStyle.info("Tél      :", telephone);
+        ConsoleStyle.info("Email    :", email);
+        ConsoleStyle.info("Année    :", String.valueOf(anneeCreation));
+        ConsoleStyle.ligneDblBas();
+    }
+}
